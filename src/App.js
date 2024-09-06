b764b65ef1e484a3f6a547cbba6a459f9b40123f
NjoguRufus <rsgxliv@gmail.com>
2024-09-06 18:28:19 +0300

    ecommerce

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductPage from './pages/ProductPage';
 import Cart from './components/Cart';
 import Checkout from './components/Checkout';
-import Contact from './components/ContactSection';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/product/:id" component={ProductPage} />
-        <Route path="/cart" component={Cart} />
-        <Route path="/checkout" component={Checkout} />
-        <Route path="/contact" component={Contact} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+      </Routes>
     </Router>
   );
 }
