5a76bcf18a8d38be031cf70ee277866418b82e8c
NjoguRufus <rsgxliv@gmail.com>
2024-09-06 21:10:55 +0300

    updt3

--- src/components/ProductsSection.js
+++ src/components/ProductsSection.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import React from 'react';
-import ProductCard from './ProductCard'; // Ensure this path is correct
-
+import ProductCard from './ProductCard';
 
 const ProductsSection = () => {
   const [products, setProducts] = useState([]);
