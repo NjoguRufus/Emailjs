9064ccea662b7250b1a662ca5448b797aefb15b1
NjoguRufus <rsgxliv@gmail.com>
2024-09-06 08:11:44 +0300

    AnimatedSection.js

--- src/components/AnimatedSection.js
+++ src/components/AnimatedSection.js
@@ -13,7 +13,8 @@ const AnimatedSection = () => {
       <div className="container mx-auto text-center">
         <h2 className="text-3xl font-bold mb-4">My Projects</h2>
         <p className="text-lg mb-8">Here are some of the projects I've worked on.</p>
-        {/* Project components will go here */}
+        {/* Project components will go here */
+        }
       </div>
     </motion.section>
   );
